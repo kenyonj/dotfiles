1c06a4002726a819662b5f6fb65aab8b0d34ae1a
Justin Kenyon <kenyonj@github.com>
2021-07-20 05:40:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ chsh -s $(which zsh)
 
 apt-get install -y \
   exa grc ripgrep shellcheck zsh-autosuggestions \
-  fuse npm fzf fasd
+  fuse npm fzf fasd ruby-dev
   
 # install latest stable node
 npm cache clean -f
@@ -22,12 +22,16 @@ wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage /usr/local/bin/nvim
 
+gem install neovim
+npm install -g neovim
+pip3 install --user neovim
+
 ln -s $(pwd)/zprofile.sh $HOME/.zprofile.sh
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/tmux.overmind.conf $HOME/.tmux.overmind.conf
 ln -s $(pwd)/aliases.zsh $HOME/.aliases.zsh
 ln -s $(pwd)/zshrc.sh $HOME/.zshrc.sh
 ln -s $(pwd)/irbrc $HOME/.irbrc
-ln -s $(pwd)/config $HOME/.config
+ln -s $(pwd)/config/nvim $HOME/.config/nvim
 
 #vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
