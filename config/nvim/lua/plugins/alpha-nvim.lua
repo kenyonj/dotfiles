bc9310c60702f421c04107ffc0530b2c067c0d26
Justin Kenyon <kenyonj@github.com>
2021-12-09 22:21:19 +0100

    Update alpha-nvim.lua

--- config/nvim/lua/plugins/alpha-nvim.lua
+++ config/nvim/lua/plugins/alpha-nvim.lua
@@ -19,7 +19,7 @@ dashboard.section.header.val = {
 
 dashboard.section.buttons.val = {
   dashboard.button("e", " New file", ":ene <BAR> startinsert<CR>"),
-  dashboard.button("f", " Find file", ":NvimTreeOpen<CR>"),
+  dashboard.button("f", " Find file", "<cmd>lua require('fzf-lua').files()<CR>"),
   dashboard.button("s", " Settings", ":e $MYVIMRC<CR>"),
   dashboard.button("u", " Update plugins", ":PackerUpdate<CR>"),
   dashboard.button("q", " Quit", ":qa<CR>"),
