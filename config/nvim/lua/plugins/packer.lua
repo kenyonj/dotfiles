87e942eb3aa3a4c5d986af2d973fc4376ab37ad3
Justin Kenyon <kenyonj@github.com>
2021-12-09 13:39:23 +0100

    Update packer.lua

--- config/nvim/lua/plugins/packer.lua
+++ config/nvim/lua/plugins/packer.lua
@@ -1,18 +1,5 @@
 vim.cmd [[packadd packer.nvim]]
 
-local fn = vim.fn
-local install_path = "~/.local/share/nvim/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
-end
-
 return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
   use "kyazdani42/nvim-tree.lua"
