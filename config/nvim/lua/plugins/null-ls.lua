f7ce7045fab47b6723b697e4e36a55be4b61c140
Justin Kenyon <kenyonj@github.com>
2021-12-13 09:41:39 +0100

    Update null-ls.lua

--- config/nvim/lua/plugins/null-ls.lua
+++ config/nvim/lua/plugins/null-ls.lua
@@ -4,8 +4,8 @@ local sources = {
   null_ls.builtins.formatting.prettier,
   null_ls.builtins.diagnostics.write_good,
   null_ls.builtins.code_actions.gitsigns,
-  null_ls.builtins.formatting.rubocop.with({ command = "/usr/local/bin/rubocop" }),
-  null_ls.builtins.diagnostics.rubocop.with({ command = "/usr/local/bin/rubocop" }),
+  null_ls.builtins.formatting.rubocop,
+  null_ls.builtins.diagnostics.rubocop,
   null_ls.builtins.formatting.gofmt,
 }
 
