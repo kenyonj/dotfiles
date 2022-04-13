14aefaf0957010009f76550f91ee68fbcfd589e4
Justin Kenyon <kenyonj@gmail.com>
2022-04-13 10:56:42 +0200

    Update for mac

--- scripts/macos-install.sh
+++ scripts/macos-install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+brew bundle
+
+xcode-select --install
+
+wget https://github.com/BlakeWilliams/remote-development-manager/releases/latest/download/rdm-darwin-arm64
+mv rdm-darwin-arm64 /opt/homebrew/bin/rdm
+chmod +x /opt/homebrew/bin/rdm
+
 dotfiles=(
   aliases.zsh
   config
@@ -24,10 +32,12 @@ npm_packages_needed=(
   vscode-langservers-extracted
   write-good
 )
-/usr/local/bin/npm install -g "${npm_packages_needed[@]}"
+/opt/homebrew/bin/npm install -g "${npm_packages_needed[@]}"
 
-HEADLESS_NEOVIM=1 /usr/local/bin/nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+HEADLESS_NEOVIM=1 /opt/homebrew/bin/nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 tmux new -d '~/.tmux/plugins/tpm/scripts/install_plugins.sh'
 
 gh extensions install mislav/gh-branch
 gh extensions install vilmibm/gh-user-status
+
+$(brew --prefix)/opt/fzf/install --all
