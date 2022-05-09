125c9649b861e35a7bb02f6b4ee90524fa9f6aaa
Justin Kenyon <kenyonj@gmail.com>
2022-05-09 15:16:19 +0200

    Update null-ls.lua

--- config/nvim/lua/plugins/null-ls.lua
+++ config/nvim/lua/plugins/null-ls.lua
@@ -30,8 +30,8 @@ local on_attach = function(client, bufnr)
   -- buf_map(bufnr, "n", "<Leader>a", ":LspDiagLine<CR>")
   buf_map(bufnr, "i", "<C-x><C-x>", "<cmd> LspSignatureHelp<CR>")
 
-  if client.resolved_capabilities.document_formatting then
-    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+  if client.server_capabilities.documentFormatting then
+    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format()")
   end
 end
 
