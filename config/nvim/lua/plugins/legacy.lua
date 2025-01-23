54d1f538d43615181a302b88191e9c52d98ab08a
Justin Kenyon <kenyonj@github.com>
2025-01-23 15:23:15 -0500

    Update legacy.lua

--- config/nvim/lua/plugins/legacy.lua
+++ config/nvim/lua/plugins/legacy.lua
@@ -1,3 +1,24 @@
+local function get_home_directory()
+  local handle = io.popen("whoami")
+  if handle then
+    local username = handle:read("*l")
+    handle:close()
+    return "/home/" .. username
+  end
+end
+
+local function get_dark_mode_status()
+  local home_directory = get_home_directory()
+  local status_file = home_directory .. "/dark_mode_status.txt"
+  local file = io.open(status_file, "r")
+  if file then
+    local status = file:read("*l"):match("^%s*(.-)%s*$")
+    file:close()
+    return status
+  end
+  return nil
+end
+
 return {
   { 'tpope/vim-eunuch',          lazy = false },
   { 'tpope/vim-rails',           lazy = false },
@@ -31,12 +52,20 @@ return {
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", {})
+        local dark_mode = get_dark_mode_status()
         vim.cmd("colorscheme gruvbox")
+
+        if dark_mode ~= "off" then
+          vim.api.nvim_set_option_value("background", "dark", {})
+        end
       end,
       set_light_mode = function()
-        vim.api.nvim_set_option_value("background", "light", {})
+        local dark_mode = get_dark_mode_status()
         vim.cmd("colorscheme gruvbox")
+
+        if dark_mode == "off" then
+          vim.api.nvim_set_option_value("background", "light", {})
+        end
       end,
     },
   },
