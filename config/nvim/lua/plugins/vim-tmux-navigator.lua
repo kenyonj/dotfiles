695abc8c1b867e4b4dfee50e309cb0d448775427
Justin Kenyon <kenyonj@gmail.com>
2025-04-11 13:37:28 -0400

    add config for vim-tmux-navigator

---
+++ config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,10 @@
+return {
+	"christoomey/vim-tmux-navigator",
+	keys = {
+		{ "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Go to the previous pane" },
+		{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Got to the left pane" },
+		{ "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Got to the down pane" },
+		{ "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Got to the up pane" },
+		{ "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Got to the right pane" },
+	},
+}
