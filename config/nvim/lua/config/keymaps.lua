0d437a8c7cdd95f09d5248f1f49cfba4445529b1
Justin Kenyon <kenyonj@github.com>
2025-04-16 12:56:50 -0400

    Update keymaps.lua

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "-", function()
+  require("snacks.explorer").open()
+end, { desc = "Open Snacks Explorer in file dir" })
+
+vim.keymap.set("n", "<A-k>", "<esc>k", { desc = "Move up" })
+vim.keymap.set("n", "<A-j>", "<esc>j", { desc = "Move down" })
+vim.keymap.set("i", "<A-k>", "<esc>gk", { desc = "Move up" })
+vim.keymap.set("i", "<A-j>", "<esc>gj", { desc = "Move down" })
+vim.keymap.set("v", "<A-k>", "<esc>gk", { desc = "Move up" })
+vim.keymap.set("v", "<A-j>", "<esc>gj", { desc = "Move down" })
