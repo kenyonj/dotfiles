fc209338b577c36842f666a899ba60e77b629be8
Justin Kenyon <kenyonj@github.com>
2025-05-05 09:55:36 -0400

    Update colorscheme.lua

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.zenbones_darken_comments = 75
+      vim.cmd.colorscheme("zenburned")
+    end,
   },
 }
